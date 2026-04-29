11ac52b14fb435cc4624e7e2a96a7c75a5ad748d
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-04-29 13:14:44 -0400

    Put AppHeader in its own file

---
+++ app/src/main/java/com/example/talkieai/widgets/AppHeader.kt
@@ -0,0 +1,31 @@
+package com.example.talkieai.widgets
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AppHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary)
+    ) {
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = "LeanlyAI",
+            color = Color.Black,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
