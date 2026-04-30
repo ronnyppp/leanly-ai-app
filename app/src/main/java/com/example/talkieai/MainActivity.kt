d37779143d05e1d46018e2686723730e311a078d
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-04-29 23:18:03 -0400

    Added navigation for buttons

--- app/src/main/java/com/example/talkieai/MainActivity.kt
+++ app/src/main/java/com/example/talkieai/MainActivity.kt
@@ -6,23 +6,55 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
+import com.example.talkieai.models.ScreenModel
 import com.example.talkieai.ui.theme.TalkieAITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val chatViewModel = ViewModelProvider(this)[ChatViewModel::class.java]
         setContent {
             TalkieAITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ChatPage(modifier = Modifier.padding(innerPadding), chatViewModel)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = MaterialTheme.colorScheme.primary
+                ) {innerPadding ->
+                    App(modifier = Modifier.padding(innerPadding), chatViewModel)
+                    //ChatPage(modifier = Modifier.padding(innerPadding), chatViewModel)
                 }
             }
         }
     }
 }
 
+@Composable
+fun App(modifier: Modifier = Modifier, viewModel: ChatViewModel) {
+    var currentScreen by remember { mutableStateOf(ScreenModel.HOME) }
+
+    when (currentScreen) {
+        ScreenModel.HOME -> HomePage(
+            onNavigate = { currentScreen = it }
+        )
+        ScreenModel.CHAT -> ChatPage(
+            viewModel = viewModel
+        )
+        ScreenModel.WORKOUT -> ChatPage(
+            viewModel = viewModel
+        )
+        ScreenModel.NUTRITION -> ChatPage(
+            viewModel = viewModel
+        )
+    }
+}
