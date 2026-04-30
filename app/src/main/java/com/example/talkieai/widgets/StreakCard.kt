6e3c867c7b636fedfee13ba1bfb960846cded6d0
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-04-30 19:04:48 -0400

    Added elevation and styling

--- app/src/main/java/com/example/talkieai/widgets/StreakCard.kt
+++ app/src/main/java/com/example/talkieai/widgets/StreakCard.kt
@@ -2,19 +2,15 @@ package com.example.talkieai.widgets
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -25,6 +21,8 @@ fun StreakCard(streak: Int) {
 
     Card(
         modifier = Modifier.size(120.dp),
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.cardElevation(6.dp),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.onPrimary
         )
