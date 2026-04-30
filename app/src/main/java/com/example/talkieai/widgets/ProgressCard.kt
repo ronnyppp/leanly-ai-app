6ecfc9130b2fb4a1b9d9d932ff5632dfbe75838a
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-04-30 19:05:10 -0400

    Added Progress Card

--- app/src/main/java/com/example/talkieai/widgets/ProgressCard.kt
+++ app/src/main/java/com/example/talkieai/widgets/ProgressCard.kt
@@ -1,2 +1,51 @@
 package com.example.talkieai.widgets
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProgressCard(
+    calories: Int = 1800,
+    calorieGoal: Int = 2200,
+    steps: Int = 6200,
+    workoutsDone: Int = 1,
+    workoutGoal: Int = 1
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.cardElevation(6.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary
+        )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+
+            Text(
+                "\uD83D\uDCAA Today’s Progress",
+                style = MaterialTheme.typography.titleMedium,
+                color = Color.Black
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text("Calories: $calories / $calorieGoal",color = Color.Black)
+            Text("Workouts: $workoutsDone / $workoutGoal",color = Color.Black)
+            Text("Steps: $steps",color = Color.Black)
+        }
+    }
+}
