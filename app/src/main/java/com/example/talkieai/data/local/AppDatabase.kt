92c1e0687d932a7bc0f00c1ae5306652d3f7f38a
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-05-03 12:50:57 -0400

    Added app database

--- app/src/main/java/com/example/talkieai/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/talkieai/data/local/AppDatabase.kt
@@ -1,2 +1,14 @@
 package com.example.talkieai.data.local
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.talkieai.models.StreakEntity
+import com.example.talkieai.models.WeightEntry
+
+@Database(
+    entities = [WeightEntry::class, StreakEntity::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun dao(): LeanlyDao
+}
