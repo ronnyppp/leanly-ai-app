83cb573ae721c2a80f1b7cfeec961c1ad8a4a620
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-05-03 16:40:10 -0400

    Implemented navhost and controller for navigation

--- app/src/main/java/com/example/talkieai/MainActivity.kt
+++ app/src/main/java/com/example/talkieai/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.talkieai
 
+import android.R.attr.type
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,8 +17,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.talkieai.models.ScreenModel
 import com.example.talkieai.ui.theme.TalkieAITheme
+import com.example.talkieai.viewmodels.ChatViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,20 +45,59 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun App(modifier: Modifier = Modifier, viewModel: ChatViewModel) {
-    var currentScreen by remember { mutableStateOf<ScreenModel>(ScreenModel.HOME) }
+    val navController = rememberNavController()
 
-    when (val screen = currentScreen) {
-        is ScreenModel.HOME -> HomePage(
-            modifier = modifier,
-            onNavigate = { currentScreen = it }
-        )
-        is ScreenModel.CHAT -> ChatPage(
-            modifier = modifier,
-            viewModel = viewModel,
-            initialPrompt = screen.initialPrompt,
-            onBackClick = { currentScreen = ScreenModel.HOME }
-        )
+    NavHost(
+        navController = navController,
+        startDestination = ScreenModel.HOME.route,
+        modifier = modifier
+    ) {
+        composable(ScreenModel.HOME.route) {
+            HomePage(
+                onNavigateToChat = {
+                    prompt ->
+                    val chatId = viewModel.createChat(prompt)
+                    navController.navigate(ScreenModel.CHAT.createRoute(chatId))
+                },
+                onOpenSaved = {
+                    navController.navigate(ScreenModel.SAVED_CHATS.route)
+                }
+            )
+        }
+        composable(
+            route = ScreenModel.CHAT.route,
+            arguments = listOf(navArgument("chatId") {type = NavType.StringType})
+        ) {
+            backStackEntry ->
+
+            val chatId = backStackEntry.arguments?.getString("chatId")!!
+
+            ChatPage(
+                viewModel = viewModel,
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                chatId = chatId,
+                onSaveChatClick = {
+                    viewModel.saveConversation(chatId)
+                    navController.navigate(ScreenModel.SAVED_CHATS.route)
+                }
+            )
+
+        }
+        composable(ScreenModel.SAVED_CHATS.route) {
+            SavedChatsPage(
+                viewModel = viewModel,
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onChatClick = { chat ->
+                    navController.navigate(ScreenModel.CHAT.createRoute(chat.id))
+                },
+            )
+        }
     }
 }
