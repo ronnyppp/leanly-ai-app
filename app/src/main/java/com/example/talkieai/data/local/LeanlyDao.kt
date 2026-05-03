111bbb8196555a142594bb19dab68a617993fd3c
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-05-03 12:51:45 -0400

    Added dao

--- app/src/main/java/com/example/talkieai/data/local/LeanlyDao.kt
+++ app/src/main/java/com/example/talkieai/data/local/LeanlyDao.kt
@@ -1,2 +1,25 @@
 package com.example.talkieai.data.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.talkieai.models.StreakEntity
+import com.example.talkieai.models.WeightEntry
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface LeanlyDao {
+
+    @Insert
+    suspend fun insertWeightEntry(entry: WeightEntry)
+
+    @Query("SELECT * FROM weight_entries ORDER BY `date` DESC")
+    fun getAllWeightEntries(): Flow<List<WeightEntry>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStreakEntry(entry: StreakEntity)
+
+    @Query("SELECT * FROM streak ORDER BY lastActiveDate DESC LIMIT 1")
+    fun getAllStreakEntries(): Flow<StreakEntity?>
+}
