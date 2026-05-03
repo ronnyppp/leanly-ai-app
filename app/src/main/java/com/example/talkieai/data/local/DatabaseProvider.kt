86bcd8b2ba09e979cbda66dccff2ecd866e5824b
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-05-03 12:51:23 -0400

    Added database provider

--- app/src/main/java/com/example/talkieai/data/local/DatabaseProvider.kt
+++ app/src/main/java/com/example/talkieai/data/local/DatabaseProvider.kt
@@ -1,2 +1,21 @@
 package com.example.talkieai.data.local
 
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseProvider {
+
+    private var INSTANCE: AppDatabase? = null
+
+    fun getDatabase(context: Context): AppDatabase {
+        return INSTANCE ?: synchronized(this) {
+            val instance = Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "fitness_db"
+            ).build()
+            INSTANCE = instance
+            instance
+        }
+    }
+}
