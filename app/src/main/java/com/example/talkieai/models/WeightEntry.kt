ed383270b5ca9e290c17b150f2e35ecc5794ffd0
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-05-03 12:52:44 -0400

    Added data class for weight

--- app/src/main/java/com/example/talkieai/models/WeightEntry.kt
+++ app/src/main/java/com/example/talkieai/models/WeightEntry.kt
@@ -1,4 +1,11 @@
 package com.example.talkieai.models
 
-class WeightEntry {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "weight_entries")
+data class WeightEntry(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val weight: Float,
+    val date: String
+)
