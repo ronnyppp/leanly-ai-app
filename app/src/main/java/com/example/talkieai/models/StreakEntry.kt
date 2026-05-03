db79a79193c635cd8cb073e8901683e5bc72d7c5
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-05-03 12:52:21 -0400

    Added data class for streak

--- app/src/main/java/com/example/talkieai/models/StreakEntry.kt
+++ app/src/main/java/com/example/talkieai/models/StreakEntry.kt
@@ -1,3 +1,11 @@
 package com.example.talkieai.models
 
-data class StreakEntry()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "streak")
+data class StreakEntity(
+    @PrimaryKey val id: Int = 0,
+    val currentStreak: Int,
+    val lastActiveDate: Long
+)
