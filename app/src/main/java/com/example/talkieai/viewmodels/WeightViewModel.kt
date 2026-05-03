cc87b07342cb364ef5483ecb1fb4e373180401a3
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-05-03 12:49:19 -0400

    Adjusted viewmodel to save with Room db

--- app/src/main/java/com/example/talkieai/viewmodels/WeightViewModel.kt
+++ app/src/main/java/com/example/talkieai/viewmodels/WeightViewModel.kt
@@ -1,4 +1,62 @@
 package com.example.talkieai.viewmodels
 
-class WeightViewModel {
-}
+import android.app.Application
+import android.content.Context
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.talkieai.data.local.DatabaseProvider
+import com.example.talkieai.models.StreakEntity
+import com.example.talkieai.models.WeightEntry
+import com.example.talkieai.models.WeightStorage.loadWeights
+import com.example.talkieai.models.WeightStorage.saveWeights
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+
+class WeightViewModel(application: Application) : AndroidViewModel(application){
+
+    private val dao = DatabaseProvider.getDatabase(application).dao()
+
+    val weights = dao.getAllWeightEntries()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+
+    val streak = dao.getAllStreakEntries()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+
+    fun addWeight(weight: Float) {
+        viewModelScope.launch {
+            val now = System.currentTimeMillis()
+
+            dao.insertWeightEntry(
+                WeightEntry(weight = weight, date = now.toString())
+            )
+            updateStreak(now)
+        }
+    }
+
+    private suspend fun updateStreak(now: Long) {
+        val curr = streak.value
+
+        val newStreak = if (curr == null) {
+            1
+        } else {
+            val diffDays = ((now - curr.lastActiveDate) / (1000 * 60 * 60 * 24)).toInt()
+
+            when(diffDays) {
+                0 -> curr.currentStreak
+                1 -> curr.currentStreak + 1
+                else -> 1
+            }
+            }
+        dao.insertStreakEntry(
+            StreakEntity(
+                currentStreak = newStreak,
+                lastActiveDate = now
+            )
+        )
+        }
+    }
+
