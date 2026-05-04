adb6083be0281f053628da3d13985c6ba713eb13
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-05-03 22:36:21 -0400

    Created chat dao

--- app/src/main/java/com/example/talkieai/data/local/ChatDao.kt
+++ app/src/main/java/com/example/talkieai/data/local/ChatDao.kt
@@ -1,4 +1,18 @@
 package com.example.talkieai.data.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.talkieai.models.ChatEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface ChatDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertChat(chat: ChatEntity)
+
+    @Query("SELECT * FROM chats ORDER BY timestamp DESC")
+    fun getAllChats(): Flow<List<ChatEntity>>
 }
