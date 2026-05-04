beb2b0f8bc3b27ce8c3d70fe98d557c3c8d0719f
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-05-03 22:36:51 -0400

    Adjusted database to store chat entities

--- app/src/main/java/com/example/talkieai/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/talkieai/data/local/AppDatabase.kt
@@ -1,14 +1,39 @@
 package com.example.talkieai.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.talkieai.models.StreakEntity
 import com.example.talkieai.models.WeightEntry
+import com.example.talkieai.models.ChatEntity
 
 @Database(
-    entities = [WeightEntry::class, StreakEntity::class],
-    version = 1
+    entities = [WeightEntry::class,
+        StreakEntity::class,
+        ChatEntity::class],
+    version = 2
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun dao(): LeanlyDao
+    abstract fun chatDao(): ChatDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+                    .fallbackToDestructiveMigration(false)
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
